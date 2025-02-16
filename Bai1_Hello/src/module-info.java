f980037afee647b3a73f51cb7b0706fa125eb8c3
Etihm <ngoquangduyhoai@gmail.com>
2025-02-16 23:01:09 +0700

    Ví dụ đầu tiên

---
+++ Bai1_Hello/src/module-info.java
@@ -0,0 +1,2 @@
+module Bai1_Hello {
+}
---
+++ Bai1_Hello/src/tiil/edu/XinChao.java
@@ -0,0 +1,11 @@
+package tiil.edu;
+
+public class XinChao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Tôi là Ngô Quang Duy Hoài học Java");
+
+	}
+
+}
