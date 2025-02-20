aaf579775997fec833edfe8524e6389c2334d4a8
Etihm <ngoquangduyhoai@gmail.com>
2025-02-20 08:32:36 +0700

    Update SanPham.java

--- ViDuHuongDoiTuong/src/SanPham.java
+++ ViDuHuongDoiTuong/src/SanPham.java
@@ -1,4 +1,33 @@
 
 public class SanPham {
+	// Định nghĩa các thuộc tính
+	int maSP;
+	String tenSP;
+	String loaiSP;
+	String anhSP;	// Tên file ảnh
+}
+	// Hàm tạo
+public SanPham() {
+	// Khởi tạo giá trị ban đầu 
+}
+
+public SanPham(int _maSP, String _tenSP, String _loaiSP, String _anhSP) {
+	maSP =_maSP;
+	tenSP =_tenSP;
+	loaiSP =_loaiSP;
+	anhSP =_anhSP;
+}
+public SanPham(int maSP, String tenSP, String loaiSP, String anhSP) {
+	this.maSP =maSP;
+	this.tenSP =tenSP;
+	this.loaiSP =loaiSP;
+	this.anhSP =anhSP;
+}
+public int getSP() {
+	return maSP;
+}
+public void setMaSP(int maSP) {
+	this.maSP = maSP;
+}
 
 }
