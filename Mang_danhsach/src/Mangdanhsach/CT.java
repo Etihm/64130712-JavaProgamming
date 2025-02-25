0cdb77f5cb8edaaf0234acb902622f24b07ef530
Etihm <ngoquangduyhoai@gmail.com>
2025-02-25 21:25:15 +0700

    fix_update

---
+++ Mang_danhsach/src/Mangdanhsach/CT.java
@@ -0,0 +1,5 @@
+package Mangdanhsach;
+
+public class CT {
+
+}
---
+++ Mang_danhsach/src/Mangdanhsach/Danhsach.java
@@ -0,0 +1,29 @@
+//
+//package Mangdanhsach;
+//import java.util.ArrayList;
+//public class Danhsach {
+//
+//	public static void main(String[] args) {
+//		//Khai báo danh sách sản phẩm
+//		ArrayList<SanPham> dsSanPham;
+//		//Xin mới
+//		dsSanPham = new ArrayList<SanPham>();
+//		
+//		// Nhập 3 sản phẩm
+//		SanPham sp1 = new SanPham(0,"Kem","Giải Khát",null);
+//		SanPham sp1 = new SanPham(1,"Cơm","Đồ ăn",null);
+//		SanPham sp1 = new SanPham(2,"Phở","Đồ ăn",null);
+//		
+//		//Thêm vào Array List
+//		dsSanPham.add(sp1);
+//		dsSanPham.add(sp2);
+//		dsSanPham.add(sp3);
+//		
+//		for(int i=0; i<ds.SanPham.size();i++) {
+//			SanPham sp = dsSanPham.get(i);
+//			System.out.println(dsSanPham.get(i).toString());
+//		}
+//
+//	}
+//
+//}
